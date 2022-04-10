98137926a2d55f47e60c1738af0d6d8c8be5a972
PPS <pavlina.psarsky@gmail.com>
2022-04-10 21:07:45 +0200

    desktop layout finished, share button and overlays working

---
+++ index.js
@@ -0,0 +1,8 @@
+const shareButton = document.querySelector('.share-button');
+
+const shareNavigation = document.querySelector('.share__navigation');
+
+shareButton.addEventListener('click', () => {
+    shareNavigation.classList.toggle('active');
+    shareButton.classList.toggle('active');
+});
